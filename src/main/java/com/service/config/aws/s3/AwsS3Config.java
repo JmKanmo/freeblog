37d1d459a71004005a216f29eda1538d2f62457d
JmKanmo <nebi25@naver.com>
2022-09-09 00:07:09 +0900

    - gitignore 추가 - aws s3 yaml 및 config 파일 수정 - category 생성순서에 따라 정렬

--- src/main/java/com/service/config/aws/s3/AwsS3Config.java
+++ src/main/java/com/service/config/aws/s3/AwsS3Config.java
@@ -1,10 +1,15 @@
 package com.service.config.aws.s3;
 
+import com.service.config.yaml.YamlPropertySourceFactory;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ConfigurationProperties(prefix = "yaml")
+@PropertySource(value = "classpath:application-util.yml", factory = YamlPropertySourceFactory.class)
 @Data
 public class AwsS3Config {
     @Value("${util-config.aws_s3-bucket.access_key_id}")
--- src/main/java/com/service/config/sftp/SFtpConfig.java
+++ src/main/java/com/service/config/sftp/SFtpConfig.java
@@ -1,10 +1,15 @@
 package com.service.config.sftp;
 
+import com.service.config.yaml.YamlPropertySourceFactory;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ConfigurationProperties(prefix = "yaml")
+@PropertySource(value = "classpath:application-util.yml", factory = YamlPropertySourceFactory.class)
 @Data
 public class SFtpConfig {
     @Value("${util-config.aws_ec2_sftp.ip}")
