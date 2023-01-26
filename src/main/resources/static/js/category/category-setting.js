8dcc8edb7170c8fa43c34aa71f8de97feb320a4e
jmkanmo <nebi25@naver.com>
2023-01-27 00:47:14 +0900

    카테고리 세팅 자바스크립트 코드 작성 (진행 中)

--- src/main/resources/static/js/category/category-setting.js
+++ src/main/resources/static/js/category/category-setting.js
@@ -4,6 +4,8 @@ class CategorySettingController extends UtilController {
         this.oneLevelCategoryButton = document.getElementById("one_level_category_button");
         this.twoLevelCategoryButton = document.getElementById("two_level_category_button");
         this.deleteCategoryButton = document.getElementById("delete_category_button");
+        this.categoryList = document.getElementById("blog_header_category_list");
+        this.prevClickedButton = null;
     }
 
     initCategorySettingController() {
@@ -11,17 +13,79 @@ class CategorySettingController extends UtilController {
     }
 
     initEventListener() {
+        this.categoryList.addEventListener("click", evt => {
+            const clickedButton = evt.target.closest("button");
+
+            if (clickedButton != null) {
+                clickedButton.style.backgroundColor = 'rgb(211 209 209)';
+
+                if (this.prevClickedButton != null && this.prevClickedButton !== clickedButton) {
+                    this.prevClickedButton.style.backgroundColor = '#ffffff';
+                }
+                this.prevClickedButton = clickedButton;
+            }
+        });
+
         this.oneLevelCategoryButton.addEventListener("click", evt => {
-            this.showToastMessage("1단 카테고리 추가");
+            const categoryValues = this.getCategoryValues(this.prevClickedButton);
+
+            if (!categoryValues) {
+                return;
+            }
+
+            console.log(categoryValues);
         });
 
         this.twoLevelCategoryButton.addEventListener("click", evt => {
-            this.showToastMessage("2단 카테고리 추가");
+            const categoryValues = this.getCategoryValues(this.prevClickedButton);
+
+            if (!categoryValues) {
+                return;
+            }
+
+            console.log(categoryValues);
         });
 
         this.deleteCategoryButton.addEventListener("click", evt => {
-            this.showToastMessage("카테고리 삭제");
+            const categoryValues = this.getCategoryValues(this.prevClickedButton);
+
+            if (!categoryValues) {
+                return;
+            }
+
+            console.log(categoryValues);
         });
+    }
+
+    getCategoryValues(category) {
+        if (!category) {
+            this.showToastMessage('대상 카테고리가 선택되지 않았습니다.');
+            return null;
+        }
+
+        let categoryValue = category.value;
+
+        if (!categoryValue) {
+            this.showToastMessage('대상 카테고리 유효성 검사에 실패하였습니다.');
+            return null;
+        }
+
+        if (category.value === 'totalCategory') {
+            return categoryValue;
+        }
+
+        categoryValue = category.value.split(":");
+
+        const categoryType = categoryValue[0];
+        const categoryValues = [];
+
+        categoryValues.push(categoryType);
+
+        categoryValue = categoryValue[1].split("&");
+
+        categoryValues.push(categoryValue[0]);
+        categoryValues.push(categoryValue[1]);
+        return categoryValues;
     }
 }
 
